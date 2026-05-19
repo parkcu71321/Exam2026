08851f6124b798f63d301ba4cf99a10c41ea37d9
parkcu71321 <parkcu71321@gmail.com>
2026-05-19 10:55:54 +0900

    9장 1. Bean Validation 예제

---
+++ src/main/java/domain/Product.java
@@ -0,0 +1,16 @@
+package domain;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class Product {
+    @NotEmpty
+    @Size(min = 4, max = 10)
+    private String name;
+
+    @Min(value = 0)
+    private int price;
+}
