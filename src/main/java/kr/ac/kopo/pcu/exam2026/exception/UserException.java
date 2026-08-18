eeb4152145f16b5083253ed3bdac0b2ccc4d154b
parkcu71321 <parkcu71321@gmail.com>
2026-08-18 11:12:26 +0900

    @ResponseStust의 2가지 방법 1)컨트롤러 요청메소드 바로 위에 어노테이션의 속성과 값을 사용해서 예외처리하는 방법 2)사용자정의 예외처리 클래스를 만들어서 예외처리는하는 방법

---
+++ src/main/java/kr/ac/kopo/pcu/exam2026/exception/UserException.java
@@ -0,0 +1,12 @@
+package kr.ac.kopo.pcu.exam2026.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "찾을 수 없습니다.")
+public class UserException extends RuntimeException{
+    public UserException(String msg) {
+        super(msg);
+        System.out.println(msg);
+    }
+}
